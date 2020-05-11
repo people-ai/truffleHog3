b54acbf2dbc26e0fc34a3485ec16d18caefb4bb9
Mike Urbanski <mike.urbanski@people.ai>
2020-05-11 14:27:02 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -9,16 +9,16 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
-    name="truffleHog3",
-    version="2.0.1",
-    packages=["truffleHog3", "truffleHog3.lib"],
+    name="pai-truffleHog3",
+    version="2.0.1.1",
+    packages=["pai-truffleHog3", "pai-truffleHog3.lib"],
     license="GNU",
     description="Find secrets in your codebase.",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/feeltheajf/truffleHog3",
-    author="Ilya Radostev",
-    author_email="feeltheajf@gmail.com",
+    url="https://github.com/peopleai/truffleHog3",
+    author="Mike Urbanski",
+    author_email="mike@people.ai",
     include_package_data=True,
     python_requires=">=3.6",
     install_requires=["GitPython==3.1.0", "jinja2==2.11.1", "PyYAML==5.3.1"],
